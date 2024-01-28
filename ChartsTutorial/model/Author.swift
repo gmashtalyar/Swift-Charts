5f58266779ca7c485daff4a63edcfaa3869ef11c
gmashtalyar <gmashtalyar@yandex.ru>
2024-01-28 07:50:51 +0300

    models

---
+++ ChartsTutorial/model/Author.swift
@@ -0,0 +1,8 @@
+//
+//  Author.swift
+//  ChartsTutorial
+//
+//  Created by Геннадий Машталяр on 28.01.2024.
+//
+
+import Foundation
---
+++ ChartsTutorial/model/BookCategory.swift
@@ -0,0 +1,8 @@
+//
+//  BookCategory.swift
+//  ChartsTutorial
+//
+//  Created by Геннадий Машталяр on 28.01.2024.
+//
+
+import Foundation
