7bdac5cc8717ca6e46435bef38f8392d40e623b0
gmashtalyar <gmashtalyar@yandex.ru>
2024-01-28 07:52:54 +0300

    models

--- ChartsTutorial/model/Author.swift
+++ ChartsTutorial/model/Author.swift
@@ -1,8 +1,21 @@
-//
-//  Author.swift
-//  ChartsTutorial
-//
-//  Created by Геннадий Машталяр on 28.01.2024.
-//
-
 import Foundation
+
+struct Author: Identifiable {
+    let id = UUID()
+    let name: String
+    
+    static var example = Author(name: "George R.R. Martin")
+    
+    static var examples = [
+        Author(name: "J.K. Rowling"),
+        Author(name: "George R.R. Martin"),
+        Author(name: "J.R.R. Tolkien"),
+        Author(name: "Agata Christie"),
+        Author(name: "Stephen King"),
+        Author(name: "Dan Brown"),
+        Author(name: "Harper Lee"),
+        Author(name: "Jane Austen"),
+        Author(name: "F. Scott Fitzgerald"),
+        Author(name: "Ernest Hemingway"),
+    ]
+}
