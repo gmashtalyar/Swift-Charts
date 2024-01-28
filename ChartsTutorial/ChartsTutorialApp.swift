48700aed00ce26e67812eabed9b9813c7c02baff
gmashtalyar <gmashtalyar@yandex.ru>
2024-01-28 06:52:17 +0300

    Initial Commit

---
+++ ChartsTutorial/ChartsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsTutorialApp.swift
+//  ChartsTutorial
+//
+//  Created by Геннадий Машталяр on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChartsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
