724b895e550cd1bb6ca941e6d4085b4e2f6ade83
gmashtalyar <gmashtalyar@yandex.ru>
2024-01-29 14:25:54 +0300

    selection and annotations

--- ChartsTutorial/view/sales stats/WeeklyChartsView.swift
+++ ChartsTutorial/view/sales stats/WeeklyChartsView.swift
@@ -4,13 +4,56 @@ import Charts
 struct WeeklyChartsView: View {
     
     @ObservedObject var salesViewModel: SalesViewModel
+    @State private var rawSelectedDate: Date? = nil
+    @Environment(\.calendar) var calendar
+    
+    var selectedDateValue: (day: Date, sales: Int)? {
+        if let rawSelectedDate {
+            return salesViewModel.salesByWeek.first {
+                let startOfWeek = $0.day
+                let endOfWeek = calendar.date(byAdding: .day, value: 6, to: startOfWeek) ?? Date()
+                return (startOfWeek...endOfWeek).contains(rawSelectedDate)
+            }
+        } else {
+            return nil
+        }
+    }
     
     var body: some View {
-        Chart(salesViewModel.salesByWeek, id: \.day) { saleDate in
-            BarMark(x: .value("Week", saleDate.day, unit: .weekOfYear),
-                    y: .value("Sales", saleDate.sales))
-            .foregroundStyle(Color.blue.gradient)
+        Chart{
+            ForEach(salesViewModel.salesByWeek, id: \.day) { saleDate in
+                BarMark(x: .value("Week", saleDate.day, unit: .weekOfYear),
+                        y: .value("Sales", saleDate.sales))
+                .foregroundStyle(Color.blue.gradient)
+                .opacity(selectedDateValue?.day == nil || selectedDateValue?.day == saleDate.day ? 1 : 0.5)
+            }
+            if let rawSelectedDate {
+                RuleMark(x: .value("Selected Date", rawSelectedDate, unit: .day)) // weekOfYear TO SHOW IN THE MIDDLE OF WEEK
+                    .foregroundStyle(Color.gray.opacity(0.3))
+                    .zIndex(-1)
+                    .annotation(position: .topTrailing, spacing: 0, overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) {
+                        selectionPopover
+                    }
+            }
         }
+        .chartXSelection(value: $rawSelectedDate)
+    }
+    
+    @ViewBuilder
+    var selectionPopover: some View {
+        if let selectedDateValue {
+            VStack {
+                Text(selectedDateValue.day.formatted(.dateTime.month().day()))
+                Text("\(selectedDateValue.sales) sales")
+            }
+            .padding(6)
+            .background {
+                RoundedRectangle(cornerRadius: 4)
+                    .fill(Color.white)
+                    .shadow(color: .blue, radius: 2)
+            }
+        }
+
     }
 }
 
